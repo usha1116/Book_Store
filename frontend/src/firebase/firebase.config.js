4a11e29e2e041b2fbf1b6091250fa55371947b88
usha1116 <ushasahu1116@gmail.com>
2025-02-06 17:18:02 +0530

    update firebase

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -6,12 +6,14 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_Auth_Domain,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyCgDGHSApowEjPhZeQDCahDocMUCwp6lU4",
+  authDomain: "mean-auth-cb617.firebaseapp.com",
+  databaseURL: "https://mean-auth-cb617-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mean-auth-cb617",
+  storageBucket: "mean-auth-cb617.appspot.com",
+  messagingSenderId: "12461046933",
+  appId: "1:12461046933:web:9823beea10fd5141d5af9c",
+  measurementId: "G-XKTMSN53WV"
 };
 
 // Initialize Firebase
